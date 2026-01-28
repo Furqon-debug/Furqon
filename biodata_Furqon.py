cf2fdca0391bda81e15a98d325700fd40ba76863
Furqon-debug <muhammadfurqonatthoriq@smkitihsanulfikri.sch.id>
2026-01-28 08:33:01 +0700

    Add simple biodata program in Python

---
+++ biodata_Furqon.py
@@ -0,0 +1,14 @@
+print("=======================================")
+print("Program Biodata Sederhana Dengan Python")
+print("=======================================")
+
+nama = "Muhammad Furqon Atthoriq"
+hobi = "Sepak Bola"
+alamat = "Karangjengkol RT03/05"
+kota = "Purwokerto"
+
+print("Nama Lengkap  :", nama)
+print("Alamat        :", alamat, "\n        Kota :", kota)
+print("Hobi          :", hobi)
+
+print("=======================================")
